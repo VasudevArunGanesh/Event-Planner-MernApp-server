f5e366f08e9d6abb08d0ea01a57c326240c3fd30
skdev2004 <137671157+skdev2004@users.noreply.github.com>
2023-11-07 17:03:53 +0530

    created event schema

--- eventSchema.js
+++ eventSchema.js
@@ -1,13 +1,44 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
 const eventSchema = new mongoose.Schema(
-  // {
-  //   name: { type: String },
-  //   email: { type: String },
-  //   password: { type: String },
-  // },
-  // {
-  //   collection: "Students",
-  // }
+  {
+    _id: ObjectId,
+    eventName: String,
+    isPublic: Boolean,
+    eventDescription: String,
+    eventType: String,
+    eventTheme: String,
+    startDate: Date,
+    endDate: Date,
+    venue: {
+      venueName: String,
+      venueAddress: String
+    },
+    organizerContact: {
+      organizerName: String,
+      organizerEmail: String,
+      organizerPhone: String
+    },
+    tickets: [
+      {
+        ticketType: String,
+        price: Number
+      }
+    ],
+    expectedAttendees: Number,
+    registrationForm: [
+      {
+        //what to write heregit n
+      }
+    ],
+    useServices: {
+      nearbyHotels: Boolean,
+      cabService: Boolean
+    }
+  }
+  
 );
 module.exports = mongoose.model("Events", eventSchema);
+// const mongoose = require("mongoose");
+
